86ed574ec84d13229d23dc7c35f9dc261fc3b29c
Julian Tusch <urhrf@student.kit.edu>
2024-11-21 16:01:30 +0100

    add scripts folder and test script

---
+++ scripts/test.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+gifts_age <- read_csv("./data/tidytuesday-2024-02-13/gifts_age.csv")
+gifts_gender <- read_csv("./data/tidytuesday-2024-02-13/gifts_gender.csv")
+historical_spending <- read_csv("./data/tidytuesday-2024-02-13/historical_spending.csv")
+
+view(gifts_age)
+view(gifts_gender)
+view(historical_spending)
