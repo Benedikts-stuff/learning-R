93dd7276d91a2c81de1a1cb86b5515906d04472c
Peter Albrecht <albrecpe@gmail.com>
2024-11-22 14:00:22 +0100

    data: cheese

---
+++ scripts/Pscript.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+cheeses <- read_csv("./data/tidytuesday-2024-06-04/cheeses.csv")
+cheese_details <- read_csv("./data/tidytuesday-2024-06-04/cheese_details.csv")
+
+view(cheeses)
+view(cheese_details)
