8e15bc9527af89b64ca1fa84438d09cf6099d656
Peter Albrecht <usnlf@student.kit.edu>
2024-12-13 16:21:21 +0100

    df-ölkggnbbnölkfgdjsjnhhlkjdfgsikujh

--- scripts/Pscript.R
+++ scripts/Pscript.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(forcats)
 library(readxl)
-library(hrbrthemes)
 
 historical_spending <- read_csv("./data/tidytuesday-2024-02-13/historical_spending.csv")
 historical_spending_labels <- levels(as_factor(historical_spending$Year))
@@ -21,5 +20,6 @@ category_labels <- levels(as_factor(longer$Category))
 longer$Category <- factor(longer$Category, levels = category_labels)
 glimpse(longer)
 ggplot(longer, aes(x = Year, y = Percentage, fill = Category)) +
-    geom_bar(stat = "identity", position = "stack", width = 0.5) +
+    geom_bar(stat = "identity", position = "fill", width = 0.5) +
+    labs(title = "Average % Spending by Category", x = "Category", y = "Average % Spending") +
     theme_minimal()
